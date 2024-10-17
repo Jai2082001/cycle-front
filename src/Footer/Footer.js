94abd5e028dfa936fabab116e451f3e3c1694717
jaideep grover <jaideepgrover147@gmail.com>
2024-10-16 23:45:08 -0400

    new

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -10,6 +10,7 @@ const Footer = () => {
                    <p className='text-lg font-semibold'> ChooseMyBicycle.com is an e-commerce portal exclusive for buying bicycles, accessories and apparels. One can pick from our wide range of top Indian and international cycling brands with also kids and toddler specific range of bicycles. Book your bicycle and sit back and relax as your dream bicycle will be delivered at your door step in a unique ready to ride box. It is also an online knowledge portal built with the purpose of helping consumers, in the market to buy a cycle , pick the right cycle based on their needs. The portal offers various tools such as a "Help Me Choose" option for amateurs looking to start cycling. Besides this, there are event listings, expert reviews, knowledge articles, tips and advice. We at ChooseMyBicycle.com will also have our own cycling events across India for all types of cyclists from amateur short weekend rides to professional race for the experts.</p>
                 </div>
                 </Col>
+                {/* sd */}
                 <Col xs={'12'} md={'3'}>
                 <div className={classes.socialMediaDiv}>
                     <h4>Follow Us On Social Media</h4>
